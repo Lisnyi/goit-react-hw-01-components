ffdc03eb7144e393fd2757c79f523c8ca99b97e9
Lisnyi <russelhobbbs@gmail.com>
2022-09-17 22:07:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { ThemeProvider } from "styled-components";
-import { theme } from "common/theme"
-import { App } from "components/App/App"
+import { theme } from "./common/theme"
+import { App } from "./components/App/App"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
